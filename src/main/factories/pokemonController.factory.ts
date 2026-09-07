848dae93877ffb374b6ed1c96b5df33f3e22d450
Andrew de Andrade <andrewalmeida2110@gmail.com>
2026-09-07 17:18:22 -0300

    :sparkles: feat: adiciona factory do PokemonController

---
+++ src/main/factories/pokemonController.factory.ts
@@ -0,0 +1,29 @@
+import { PokemonController } from "@infrastructure/http/controllers/pokemonController";
+import { InMemoryPokemonRepository } from "@infrastructure/database/in-memory/InMemoryPokemonRepository";
+import { CreatePokemonUseCase } from "@application/use-cases/createPokemon";
+import { DeletePokemonUseCase } from "@application/use-cases/deletePokemon";
+import { GetPokemonByIdUseCase } from "@application/use-cases/getPokemonById";
+import { ListPokemonsUseCase } from "@application/use-cases/listPokemons";
+import { UpdatePokemonUseCase } from "@application/use-cases/updatePokemon";
+
+export function makePokemonController(): PokemonController{
+
+    //Instância do repositório em memória
+    const pokemonRepository = new InMemoryPokemonRepository();
+
+    //Instâncias de caso de uso
+    const createPokemonUseCase = new CreatePokemonUseCase(pokemonRepository);
+    const deletePokemonUseCase = new DeletePokemonUseCase(pokemonRepository);
+    const getPokemonByIdUseCase = new GetPokemonByIdUseCase(pokemonRepository);
+    const listPokemonsUseCase = new ListPokemonsUseCase(pokemonRepository);
+    const updatePokemonUseCase = new UpdatePokemonUseCase(pokemonRepository);
+
+    //Instância final do Controller
+    return new PokemonController(
+        createPokemonUseCase,
+        deletePokemonUseCase,
+        getPokemonByIdUseCase,
+        listPokemonsUseCase,
+        updatePokemonUseCase
+    );
+}
