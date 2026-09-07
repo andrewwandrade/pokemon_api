495361f0ed4715b4a159bb65925c90d56d95ac83
Andrew de Andrade <andrewalmeida2110@gmail.com>
2026-09-07 17:15:03 -0300

    :truck: chore: move e atualiza swagger-generator

---
+++ src/main/config/swagger-generator.ts
@@ -0,0 +1,45 @@
+import path from 'path';
+import swaggerAutogen from 'swagger-autogen';
+
+const doc = {
+  info: {
+    title: 'API para gerenciamento de Pokémons',
+    version: '1.0.0',
+    description:
+      'API desenvolvida para a disciplina Tópicos Especiais em Engenharia de Software (UFF)',
+  },
+  host: 'localhost:3000',
+  basePath: '/',
+  schemes: ['http'],
+  consumes: ['application/json'],
+  produces: ['application/json'],
+  tags: [
+    {
+      name: 'Pokemons',
+      description: 'Criar, editar, atualizar e listar Pokémons',
+    },
+  ],
+  components: {
+    schemas: {
+      Pokemon: {
+        id: '1',
+        name: 'Pikachu',
+        type: 'Electric',
+        height: 0.4,
+        weight: 6.0,
+      },
+      PokemonInput: {
+        name: 'Pikachu',
+        type: 'Electric',
+        height: 0.4,
+        weight: 6.0,
+      },
+    },
+  },
+};
+
+const outputFile = path.resolve(__dirname, 'swagger-output.json');
+
+const endpointsFiles = [path.resolve(__dirname, '../../infrastructure/http/routes/pokemonRoutes.ts')];
+
+swaggerAutogen({ openapi: '3.0.0' })(outputFile, endpointsFiles, doc);
